9f4fa81ad0b1420c8f5f812159e14cecd90e199b
戴高斌（戴高斌） <daigb@uuzu.com>
2018-07-24 18:03:10 +0800

    commit

---
+++ src/router2.js
@@ -0,0 +1,29 @@
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+
+let home = new Router();
+home.get('/list',async(ctx)=>{
+    ctx.body="Home XXX";
+}).get('/todo',async(ctx)=>{
+    ctx.body ='Home ToDo';
+})
+
+let page = new Router();
+page.get('/list',async(ctx)=>{
+    ctx.body="Page XXX";
+}).get('/todo',async(ctx)=>{
+    ctx.body ='Page ToDo';
+})
+
+//装载所有子路由
+let router = new Router();
+router.use('/home',home.routes(),home.allowedMethods());
+router.use('/page',page.routes(),page.allowedMethods());
+
+//加载路由中间件
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000,()=>{
+    console.log('[demo] server is starting at port 3000');
+});
